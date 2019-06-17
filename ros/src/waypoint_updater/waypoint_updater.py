bb8c49c96f0858dc6cfa57708bcaea9d2774b820
creich <creich@linux.com>
2019-06-17 21:39:26 +0200

    first working darft of DBW node (DBW walkthrough)

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -47,7 +47,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(1)
+        rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             if self.pose and self.base_waypoints:
                 # get closest waypoint

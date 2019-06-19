377aceba53f60e49fe951b60b7adc8f648e35a89
creich <creich@linux.com>
2019-06-20 01:32:31 +0200

    removed debug prints (have been commented out already)

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,7 +1,6 @@
 from styx_msgs.msg import TrafficLight
 import tensorflow as tf
 import numpy as np
-import datetime
 
 class TLClassifier(object):
     def __init__(self, is_site):
@@ -40,30 +39,20 @@ class TLClassifier(object):
         """
         with self.graph.as_default():
             img_expand = np.expand_dims(image, axis=0)
-            #start = datetime.datetime.now()
             (boxes, scores, classes, num_detections) = self.sess.run(
                 [self.boxes, self.scores, self.classes, self.num_detections],
                 feed_dict={self.image_tensor: img_expand})
-            #end = datetime.datetime.now()
-            #duration = end - start
-            #print("time to classify: {}".format(duration.total_seconds()))
 
         boxes = np.squeeze(boxes)
         scores = np.squeeze(scores)
         classes = np.squeeze(classes).astype(np.int32)
 
-        #print("SCORES: {}".format(scores))
-        #print("CLASSES: {}".format(classes))
-
         if scores[0] > self.threshold:
             if classes[0] == 1:
-                #print('GREEN')
                 return TrafficLight.GREEN
             elif classes[0] == 2:
-                #print('RED')
                 return TrafficLight.RED
             elif classes[0] == 3:
-                #print('YELLOW')
                 return TrafficLight.YELLOW
 
         return TrafficLight.UNKNOWN
